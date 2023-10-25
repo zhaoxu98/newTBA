cdf46efb929105bab495f3223d536c9048a043bd
zxu674 <zxu674@mgmt-4.hkust-gz.edu.cn>
2023-10-25 18:40:07 +0800

    init

--- scripts/1.sh
+++
@@ -1,3 +0,0 @@
-
-
-python models/trajecotory_user_linking/main.py --dataset Chengdu_Sample12 --grid_size 120 --d_model 128 --n_heads 5 --n_layers 2 --epochs 400 --train_batch 32 --valid_batch 32 --test_batch 32
